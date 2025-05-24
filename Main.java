6aed716c7e7f2ac8bebe005b5809585532be6ae1
laquizhpe <luisangelmonike@gmail.com>
2025-05-24 14:19:33 +0200

    clase principal para refactorizar

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        double precioProducto1 = 10.5;
+        double precioProducto2 = 25.3;
+
+        double total = calcularPrecioTotal(precioProducto1, precioProducto2);
+
+        System.out.println("El precio total es: " + total);
+    }
+
+    public static double calcularPrecioTotal(double precio1, double precio2) {
+        return precio1 + precio2;
+    }
+}
